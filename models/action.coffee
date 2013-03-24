4a0f9cb7f2f533b3426992e9ccb3d76ce7de55f1
xulin <xulin97@gmail.com>
2013-03-24 15:03:55 +0800

    定义models

---
+++ models/action.coffee
@@ -0,0 +1,3 @@
+# User xulin
+# Date: 13-3-24
+# Time: 下午2:52
---
+++ models/task.js
@@ -0,0 +1,6 @@
+/**
+ * User: xulin
+ * Date: 13-3-24
+ * Time: 下午2:52
+ * To change this template use File | Settings | File Templates.
+ */
